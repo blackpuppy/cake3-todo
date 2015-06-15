e50714d4ccba3c5209281cae316b7bfd025b84bb
Zhu Ming <mingzhu.z+github@gmail.com>
2015-06-15 21:23:09 +0800

    add test fixture

---
+++ todo/tests/Fixture/TodosFixture.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class TodosFixture extends TestFixture {
+
+  public $import = ['table' => 'todos'];
+
+  public $records = [
+    [
+      'id' => 1,
+      'todo' => 'First To-do',
+      'is_done' => '0',
+      'created' => '2013-11-21 12:00:00',
+      'updated' => '2013-11-21 12:00:00'
+    ],
+    [
+      'id' => 2,
+      'todo' => 'Complete To-do',
+      'is_done' => '1',
+      'created' => '2013-11-21 12:00:00',
+      'updated' => '2013-11-21 12:00:00'
+    ]
+  ];
+}
